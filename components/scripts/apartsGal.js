97058ee14f6efc2608b9993e838fc0936404b265
itsca <itscae@gmail.com>
2017-03-01 02:20:11 -0600

    added first gallery image with paths, missing change image function

--- components/scripts/apartsGal.js
+++ components/scripts/apartsGal.js
@@ -1,28 +1,52 @@
 // Apartments Gallery
 // ==============
 
-var showGallery = function(element) {
-	var apartmentTypes = jQuery(".aparment-type"),
-		currApartment  = element,
-	    hiddenGal = jQuery(".hidden-gal"),
-	    galleryBackgrounds = [["","","","",""], ["","","","",""], ["","","","",""], ["","","","",""], ["","","","",""]];
+var galleryLocations = [["../images/aparments/A/1.jpg",
+	    						"../images/aparments/A/2.jpg",
+	    						"../images/aparments/A/3.jpg",
+	    						"../images/aparments/A/4.jpg"],
+	    						["../images/aparments/B/1.jpg",
+	    						"../images/aparments/B/2.jpg",
+	    						"../images/aparments/B/3.jpg"],
+	    						["../images/aparments/C/1.jpg",
+	    						"../images/aparments/C/2.jpg",
+	    						"../images/aparments/C/3.jpg",
+	    						"../images/aparments/C/4.jpg"],
+	    						["../images/aparments/C1/1.jpg",
+	    						"../images/aparments/C1/2.jpg",
+	    						"../images/aparments/C1/3.jpg",
+	    						"../images/aparments/C1/4.jpg"],
+	    						["../images/aparments/C2/1.jpg",
+	    						"../images/aparments/C2/2.jpg",
+	    						"../images/aparments/C2/3.jpg",
+	    						"../images/aparments/C2/4.jpg",
+	    						"../images/aparments/C2/5.jpg"]],
+	currApartmentUrls,
+	hiddenGal = jQuery(".hidden-gal"),		
+	showGallery = function(element) {
+		var apartmentTypes = jQuery(".aparment-type"),
+			currApartment  = element,
+		    firstImageDiv = hiddenGal.find(".gal")[0];
+		hiddenGal.fadeIn();
+		for (var i = 0; i < apartmentTypes.length; i++) {
+		   	if (currApartment === apartmentTypes[i]) {
+		   		currApartmentUrls = galleryLocations[i];
+		   	}
+		}
+		console.log(firstImageDiv);
+		jQuery(firstImageDiv).css('background-image', 'url(' + currApartmentUrls[0] + ')');						
+	},
+	changeGalIamge = function(nextIndex) {
+		var currGalIndex = 0;
 
-	if (windowTop >= firstDivTop - 100 &&
-	    windowTop <= lastDivBottom - jQuery(sectionTargets[numberOfSections]).height() + 50) {
-	    jQuery('#dot-nav-wrapper').fadeIn(200);
-	}else {
-	  jQuery('#dot-nav-wrapper').fadeOut(200);
 	}
-	for (var i = 0; i < sectionTargets.length; i++) {
-	  if (windowTop >= jQuery(sectionTargets[i]).offset().top - 200) {
-	    jQuery('.current').removeClass('current');
-	    jQuery(dotTargets[i]).addClass('current');
-	  };
-	};
-}
 
 
-
+jQuery('.aparment-type').click(function(event){
+  event.preventDefault();
+  showGallery(this);
+});
+/*
 if (jQuery(".aparment-type").length > 0) {
 
-}
+}*/
