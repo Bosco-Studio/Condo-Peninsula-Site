17dec9dc29f0e94bf470232385f15c348029b42e
itsca <itscae@gmail.com>
2017-03-02 18:39:57 -0600

    added gallery closer, fix movil gal

--- components/scripts/apartsGal.js
+++ components/scripts/apartsGal.js
@@ -1,26 +1,26 @@
 // Apartments Gallery
 // ==============
 
-var galleryLocations = [["../images/aparments/A/1.jpg",
-	    						"../images/aparments/A/2.jpg",
-	    						"../images/aparments/A/3.jpg",
-	    						"../images/aparments/A/4.jpg"],
-	    						["../images/aparments/B/1.jpg",
-	    						"../images/aparments/B/2.jpg",
-	    						"../images/aparments/B/3.jpg"],
-	    						["../images/aparments/C/1.jpg",
-	    						"../images/aparments/C/2.jpg",
-	    						"../images/aparments/C/3.jpg",
-	    						"../images/aparments/C/4.jpg"],
-	    						["../images/aparments/C1/1.jpg",
-	    						"../images/aparments/C1/2.jpg",
-	    						"../images/aparments/C1/3.jpg",
-	    						"../images/aparments/C1/4.jpg"],
-	    						["../images/aparments/C2/1.jpg",
-	    						"../images/aparments/C2/2.jpg",
-	    						"../images/aparments/C2/3.jpg",
-	    						"../images/aparments/C2/4.jpg",
-	    						"../images/aparments/C2/5.jpg"]],
+var galleryLocations = [["images/aparments/A/1.jpg",
+						"images/aparments/A/2.jpg",
+						"images/aparments/A/3.jpg",
+						"images/aparments/A/4.jpg"],
+						["images/aparments/B/1.jpg",
+						"images/aparments/B/2.jpg",
+						"images/aparments/B/3.jpg"],
+						["images/aparments/C/1.jpg",
+						"images/aparments/C/2.jpg",
+						"images/aparments/C/3.jpg",
+						"images/aparments/C/4.jpg"],
+						["images/aparments/C1/1.jpg",
+						"images/aparments/C1/2.jpg",
+						"images/aparments/C1/3.jpg",
+						"images/aparments/C1/4.jpg"],
+						["images/aparments/C2/1.jpg",
+						"images/aparments/C2/2.jpg",
+						"images/aparments/C2/3.jpg",
+						"images/aparments/C2/4.jpg",
+						"images/aparments/C2/5.jpg"]],
 	currApartmentUrls,
 	hiddenGal = jQuery(".hidden-gal"),
 	currGalIndex;		
@@ -37,6 +37,9 @@ var galleryLocations = [["../images/aparments/A/1.jpg",
 		}
 		console.log(firstImageDiv);
 		jQuery(firstImageDiv).css('background-image', 'url(' + currApartmentUrls[currGalIndex] + ')');						
+	},
+	hideGallery = function() {
+		hiddenGal.fadeOut();						
 	},
 	changeGalIamge = function(nextIndex) {
 		var nextGalIndex = currGalIndex + nextIndex,
@@ -70,6 +73,10 @@ jQuery('.slider-left').click(function(){
 
 jQuery('.slider-right').click(function(){
   changeGalIamge(1);
+});
+
+jQuery('.gallery-closer').click(function(){
+  hideGallery();
 });
 
 /*
